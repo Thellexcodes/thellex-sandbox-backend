51c783767f3ea4b1ab53abc06082496216aba9d2
thellecodes <60751900+thellecodes@users.noreply.github.com>
2025-09-11 11:17:30 +0300

    fixed bug on wallet activation

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller.v1';
+import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '@/utils/typeorm/entities/user.entity';
 import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
